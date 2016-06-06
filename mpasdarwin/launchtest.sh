1683a8c75ec80959a44f48f9d21ae0cd0c650487
guanxyz <guanxyz82@gmail.com>
2016-06-06 11:06:02 -0600

    Fixed the UUID in VPIC

--- mpasdarwin/launchtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python launch_cluster.py template_qgmpas.xml /projects/groups/vizproject/VMs/qgmpasubu16.qcow2 ./tmp --node cn186 --node cn187 --verbose 
-#python launch_cluster.py template_qgmpas.xml /projects/groups/vizproject/VMs/test222.qcow2 ./tmp --node cn180 --node cn181 --verbose 
-
